a037bfadf5f41cbebe2846daa2d4161d8ada3f60
paiotti <rodrigopaiotti96@gmail.com>
2024-06-05 11:34:44 -0300

    refactoring some code

--- src/handlers/player.js
+++ src/handlers/player.js
@@ -1,15 +1,32 @@
 import { loadData, saveData } from "./fsFuncionality.js";
 
-export async function addPlayer(nickname, mmr, league, availability, gameClass) {
-	let players = await loadData()
-	const playersCount = Object.keys(players).length
+const leagues = [
+	{ max: 40, league: 'liverto' },
+	{ max: 60, league: 'liverto+' },
+	{ max: 81, league: 'kzarka' },
+	{ max: 121, league: 'kzarka+' },
+	{ max: 161, league: 'blackstar' },
+	{ max: Infinity, league: 'blackstar+' },
+];
 
-	let playerExist = false
-	for(let i = 0; i < playersCount; i++){
-		if(nickname == players[i].nickname) playerExist = true
+function getLeague(mmr) {
+
+	for (const range of leagues) {
+			if (mmr < range.max) {
+					return range.league;
+			}
+	}
+}
+
+export async function addPlayer(nickname, mmr, availability, gameClass, league = 'default') {
+	let players = await loadData();
+
+	if (players.some(player => player.nickname === nickname)) {
+			console.log('Player already exists');
+			return;
 	}
 
-	if(playerExist) return console.log('player already created');
+	if(league === 'default') league = getLeague(mmr);
 
 	players[playersCount] = {
 		"nickname": nickname,
@@ -23,15 +40,39 @@ export async function addPlayer(nickname, mmr, league, availability, gameClass) 
 	await saveData(players)
 	console.log(`player ${nickname} created successfully`);
 }
-export async function editPlayer(nickname) {
+export async function editPlayer(identifier, fiedl, newValue, searchBy = 'nickname') {
 	let players = await loadData()
-	const playersCount = Object.keys(players).length
+	let playerId = null
 
-	let playerId = '0'
-	for(let i = 0; i < playersCount; i++){
-		if(nickname == players[i].nickname) playerId = i
+	if (searchBy == 'nickname' ) {
+		playerId = players.findIndex(player => player.nickname == identifier)
+	} else if (searchBy == 'id'){
+		playerId = identifier
 	}
-	
-	console.log(players[playerId]);
+
+	switch (fiedl) {
+		case 'nickname':
+			players[playerId].nickname = newValue
+			break;
+		case 'mmr':
+			players[playerId].mmr = newValue
+			players[playerId].league = getLeague(newValue)
+			break;
+		case 'availability':
+			players[playerId].availability = newValue
+			break;
+		case 'gameClass':
+			players[playerId].gameClass = newValue
+			break;
+		default:
+			return console.log('Field not valid');
+	}
+
+	await saveData(players)
+
 }
-export async function checkInPlayer() {}
+export async function checkInPlayer(id, check) {
+	let players = await loadData()
+	if(typeof check != "boolean") return console.log('not valid check');
+	players[id].checkIn = check
+}
