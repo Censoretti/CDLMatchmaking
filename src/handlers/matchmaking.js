fea83db722aa939e2f12ca31f58a714c73c11477
paiotti <rodrigopaiotti96@gmail.com>
2024-06-07 19:48:19 -0300

    small uptade

--- src/handlers/matchmaking.js
+++ src/handlers/matchmaking.js
@@ -2,6 +2,7 @@ import { loadData } from "./fsFuncionality.js";
 
 export async function matchmaking() {
 	const players = await loadData()
+	let ranges = {}
 	for(const range of Object.keys(players)){
 		console.log(players[range]);
 	}
