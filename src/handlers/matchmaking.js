fd3b5b92da54506f0d3e29096621e0bbbff82405
paiotti <rodrigopaiotti96@gmail.com>
2024-06-10 15:16:14 -0300

    initial machmaking system

--- src/handlers/matchmaking.js
+++ src/handlers/matchmaking.js
@@ -4,6 +4,12 @@ export async function matchmaking() {
 	const players = await loadData()
 	let ranges = {}
 	for(const range of Object.keys(players)){
-		console.log(players[range]);
+		// console.log(players[range].availability);
+		for(const hours of players[range].availability){
+			if(ranges[hours] == undefined) ranges[hours] = []
+			// console.log(players[range].nickname);
+			ranges[hours].push(players[range].nickname)
+		}
 	}
+	console.log(ranges);
 }
