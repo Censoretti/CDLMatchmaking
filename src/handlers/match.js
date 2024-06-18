7442799937335954b2958e40c70f6587eb352e05
paiotti <rodrigopaiotti96@gmail.com>
2024-06-18 16:46:11 -0300

    starting match system

--- src/handlers/match.js
+++ src/handlers/match.js
@@ -1,16 +1,25 @@
 import { loadData, saveData } from "./fileSystem.js";
 
 class match {
-	constructor() {
-		this.id
+	constructor(id, hour, day, player1Nickname, player2Nickname) {
+		this.id = id
 		this.date = {
-			time,
-			day
+			time: hour,
+			day: day
 		}
-		this.player1
-		this.player2
-		this.winner
-		this.vod
+		this.player1 = player1Nickname
+		this.player2 = player2Nickname
+		this.result = {
+			winner: '',
+			round1: '',
+			round2: '',
+			round3: ''
+		}
+		this.vod = ''
+	}
+
+	win(player){
+		console.log(player);
 	}
 }
 
